7f7dea4972e4855c8e287ef3168e134782fa38a5
koalaProgrammer <ruoyw0@gmail.com>
2014-01-16 01:01:33 -0800

    Initialized the Repo
    Trying my hands at programming a simple text based adventure game. I
    kind of want to get more into graphical stuff, and this might be a good
    opportunity to do that since I only need simple graphical things.

---
+++ src/Adventure/Character.java
@@ -0,0 +1,14 @@
+package Adventure;
+
+/** The Character class will be used to represent
+ *  a character, where it will keep track of information
+ *  on the character such as the name, age, and so on,
+ *  as well as other skills and so on. Might make this 
+ *  an Abstract class to have different classes of characters
+ *  that have their own methods.
+ * @author Ruoyang Wang
+ *
+ */
+public class Character {
+
+}
---
+++ src/Adventure/Main.java
@@ -0,0 +1,11 @@
+package Adventure;
+
+/** My first attempt at an adventure game. I will probably
+ * start with the command line things, but later add in some
+ * sort of intefrace and simple graphics to make it more accessible
+ * @author Ruoyang Wang
+ *
+ */
+public class Main {
+
+}
